6470f45d92e7c3c07dc290de338ba033137b7dca
Olenev Savely <114435690+Somewhere0verTheRainbow@users.noreply.github.com>
2022-10-25 23:59:55 +0400

    Pagination (#5)
    Co-authored-by: sotiredofyours <h8everrything@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"animes/:id", component:CardsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/card-poster/card-poster.component.ts
+++ src/app/card-poster/card-poster.component.ts
@@ -1,31 +1,35 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { withDisabledInitialNavigation } from '@angular/router';
-import {AnimeFromList} from "../models/AnimeFromList";
+import { AnimeFromList } from '../models/AnimeFromList';
 import { KindType } from '../models/AnimeTypes';
 
 @Component({
   selector: 'app-card-poster',
   templateUrl: './card-poster.component.html',
-  styleUrls: ['./card-poster.component.css']
+  styleUrls: ['./card-poster.component.css'],
 })
 export class CardPosterComponent implements OnInit {
   @Input() anime!: AnimeFromList;
-  date!:Date;
-  type!:string;
+  date!: Date;
+  type!: string;
+  name!: string;
 
   constructor() {}
 
   ngOnInit(): void {
-     this.date = new Date(this.anime.aired_on)
-     if (this.anime.kind == KindType.tv || this.anime.kind == KindType.tv_13
-         || this.anime.kind == KindType.tv_24
-         || this.anime.kind == KindType.tv_48 )
-        {
-            this.type = "TV Сериал"
-        }
-    else{
-        this.type = this.anime.kind.toString()
+    this.date = new Date(this.anime.aired_on);
+    if (
+      this.anime.kind == KindType.tv ||
+      this.anime.kind == KindType.tv_13 ||
+      this.anime.kind == KindType.tv_24 ||
+      this.anime.kind == KindType.tv_48
+    )
+      this.type = 'TV Сериал';
+    else {
+      this.type = this.anime.kind.toString();
     }
+    if (this.anime.kind == KindType.movie) this.type = 'Фильм';
+    if (this.anime.russian) this.name = this.anime.russian;
+    else this.name = this.anime.name;
   }
-
 }
