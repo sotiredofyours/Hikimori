bbb32feaf23f2f76c6d3080c3ee8cfc75b8a241d
sotiredofyours <h8everrything@gmail.com>
2022-10-28 00:16:48 +0400

    add routes, change some methods in cards.ts

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -3,50 +3,77 @@ import {AnimeFromList} from "../models/AnimeFromList";
 import {AnimeDataService} from "../services/ShikimoriAPI/anime-data.service";
 import {OrderType} from "../models/AnimeTypes";
 import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {RequestParams} from "../models/RequestParams";
 
 @Component({
-	selector: 'app-cards',
-	templateUrl: './cards.component.html',
-	styleUrls: ['./cards.component.css']
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
 })
 
 
 export class CardsComponent implements OnInit {
+
   titleName: string = 'Лучшие аниме';
-	animes: AnimeFromList[] = [];
+  animes: AnimeFromList[] = [];
   nextPage: AnimeFromList[] = [];
-	page!: number;
+  page: number = 1;
 
   isAnons:boolean = false;
-  isOngoing:boolean = false;
-  isReleased:boolean = false;
+  //filters
+  status: string[] = [];
 
+  constructor(private animeDataService: AnimeDataService, private route: ActivatedRoute, private router: Router) {}
 
-	constructor(private animeDataService: AnimeDataService, private route: ActivatedRoute, private router:Router) { }
-
-	ngOnInit(): void {
-		this.page = Number(this.route.snapshot.paramMap.get('page'));
-		this.getAnimeFromList();
-    this.router.events.subscribe(val =>{
-      if (val instanceof NavigationEnd){
-        this.getAnimeFromList();
+  ngOnInit(): void {
+    this.getAnimeFromList(this.getParams());
+    this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) {
+        this.getAnimeFromList(this.getParams());
       }
     })
-	}
+  }
 
-	getAnimeFromList(): void {
-		this.animeDataService.getAnimeList({limit:50, order:OrderType.ranked, page:this.page})
-			.subscribe({ next: (data: AnimeFromList[]) => this.animes = data });
-    this.animeDataService.getAnimeList({limit:50, order:OrderType.ranked, page:this.page+1})
-      .subscribe({next: (data: AnimeFromList[]) => this.nextPage = data});
-	}
+  getParams():RequestParams{
+    let params:RequestParams = {limit:50, order: OrderType.ranked }
+    let filter = this.route.snapshot.paramMap.get('type');
+    let page =Number(this.route.snapshot.paramMap.get('page'));
+    if (filter) params.status = filter;
+    if (page) {
+      this.page = page;
+      params.page = page
+    }
+    console.log(JSON.stringify(params))
+    return params;
+  }
 
-	pageUp(): void{
-		this.router.navigate(['/animes', ++this.page]);
-	}
+  getAnimeFromList(params:RequestParams): void {
 
-	pageDown():void{
-		this.router.navigate(['/animes', --this.page]);
-	}
+    this.animeDataService.getAnimeList(params)
+      .subscribe({
+        next: (data: AnimeFromList[]) => this.animes = data
+      });
+    params.page = params.page!+1;
+    this.animeDataService.getAnimeList(params)
+      .subscribe({
+        next: (data: AnimeFromList[]) => this.nextPage = data
+      });
+  }
+
+  pageUp(): void {
+    this.router.navigate(['/animes', ++this.page]);
+  }
+
+  pageDown(): void {
+    this.router.navigate(['/animes', --this.page]);
+  }
+
+  addAnons(){
+    this.status.push('anons');
+    let url = this.status.join(',')
+    this.router.navigate([`/animes/status/${url}/1`]);
+    this.isAnons = !this.isAnons;
+  }
+
 
 }
