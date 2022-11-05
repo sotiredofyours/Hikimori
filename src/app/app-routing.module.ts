17392ebcaf5a3c39986562807ea7c062111301a8
sotiredofyours <h8everrything@gmail.com>
2022-11-05 13:25:42 +0400

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {CardsComponent} from './cards/cards.component';
+import {PAnimeComponent} from "./p-anime/p-anime.component";
 
 const routes: Routes = [
   {path: "animes/:page", component: CardsComponent},
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path: "animes/:filter1/:filter1-type/:filter2/:filter2-type/:filter3/:filter3-type/:filter4/:filter4-type/:page", component:CardsComponent},
   {path: "animes/:filter1/:filter1-type/:filter2/:filter2-type/:filter3/:filter3-type/:filter4/:filter4-type/:filter5/:filter5-type/:page", component:CardsComponent},
   {path: "animes/:filter1/:filter1-type/:filter2/:filter2-type/:filter3/:filter3-type/:filter4/:filter4-type/:filter5/:filter5-type/:filter6/:filter6-type/:page", component:CardsComponent},
+  {path: "anime/:id", component: PAnimeComponent}
 ];
 
 @NgModule({
